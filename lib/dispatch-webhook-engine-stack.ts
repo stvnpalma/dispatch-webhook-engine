bac2499c5ff313534cf46a0bf721f8026b6ab1b0
Steven Palma <141971217+stvnpalma@users.noreply.github.com>
2026-08-02 12:37:36 -0400

    feat(dynamodb): add EventsTable and SubscriptionsTable with GSI and unit tests (#3)
    Implement EventsTable with status GSI and SubscriptionsTable with composite primary key in the CDK stack, along with corresponding unit tests enforcing key schema and billing mode.

--- lib/dispatch-webhook-engine-stack.ts
+++ lib/dispatch-webhook-engine-stack.ts
@@ -1,16 +1,42 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as cdk from "aws-cdk-lib/core";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class DispatchWebhookEngineStack extends cdk.Stack {
+  public readonly eventsTable: dynamodb.Table;
+  public readonly subscriptionsTable: dynamodb.Table;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    this.eventsTable = new dynamodb.Table(this, "EventsTable", {
+      partitionKey: {
+        name: "eventId",
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+    this.eventsTable.addGlobalSecondaryIndex({
+      indexName: "StatusIndex",
+      partitionKey: {
+        name: "status",
+        type: dynamodb.AttributeType.STRING,
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DispatchWebhookEngineQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    this.subscriptionsTable = new dynamodb.Table(this, "SubscriptionsTable", {
+      partitionKey: {
+        name: "subscriberId",
+        type: dynamodb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: "eventType",
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
   }
 }
